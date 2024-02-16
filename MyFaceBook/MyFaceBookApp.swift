2f667a8ab26712525416cd619ddace73c14f73c2
jrderanian <105163216+jrderanian@users.noreply.github.com>
2024-02-16 17:49:22 -0600

    Initial Commit

---
+++ MyFaceBook/MyFaceBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFaceBookApp.swift
+//  MyFaceBook
+//
+//  Created by John Deranian on 2/16/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFaceBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
